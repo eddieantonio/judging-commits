bc9cdc697c41eb4272131adeb4bbd6f2b1eb587a
Eddie Antonio Santos <easantos@ualberta.ca>
2016-02-06 15:18:49 -0700

    Embetter get_statuses.rb.

--- get_statuses.rb
+++ get_statuses.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
 
+# Copyright 2016 Eddie Antonio Santos <easantos@ualberta.ca>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'tqdm'
 require 'octokit'
 require 'csv'
@@ -8,6 +22,12 @@ def token
   File.open 'gh-access-token' do |file|
     file.read.chomp
   end
+rescue Errno::ENOENT => e
+  $stderr.puts <<-EOF
+    Create a GitHub Access token and save it as a text file in the current
+    working directory called 'gh-access-token'
+  EOF
+  raise e
 end
 
 Client = Octokit::Client.new(access_token: token)
@@ -48,7 +68,7 @@ Commit = Struct.new(:repo, :sha) do
 end
 
 CSV.open('commit-status.csv', 'wb') do |output|
-  CSV.open('data.csv', 'rb', headers: true, encoding: 'UTF-8') do |infile|
+  CSV.open('commits.csv', 'rb', headers: true, encoding: 'UTF-8') do |infile|
     infile.to_enum.with_progress.each do |row|
       commit = Commit.new(row[0], row[1])
       status = commit.fetch_status
